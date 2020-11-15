3330148f5fed26e40e1ac8a108748fafacef2767
Sandeep <sxk80270@ucmo.edu>
2020-11-15 01:14:32 -0500

    Rendering Multiple Dynamic Templates

--- src/DjangoProject/urls.py
+++ src/DjangoProject/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import (
+    home_page,
+    about_us,
+    contact_us
+)
 
 urlpatterns = [
     path('', home_page),
+    path('about-us', about_us),
+    path('contact-us', contact_us),
     path('admin/', admin.site.urls),
 ]
--- src/DjangoProject/views.py
+++ src/DjangoProject/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    my_title = "Welcome to Home Page"
+    return render(request, "home_page.html", {"title": my_title})
+
+
+def about_us(request):
+    my_title = "Welcome to AboutUs Page"
+    return render(request, "home_page.html", {"title": my_title})
+
+
+def contact_us(request):
+    my_title = "Welcome to ContactUs Page"
+    return render(request, "home_page.html", {"title": my_title})
